18ed092ec577876eebf76fc487102ad261015b84
Oliver King <116508612+oliverk1@users.noreply.github.com>
2022-11-23 10:07:56 +0000

    Update PySimpleGUITest.py

--- PySimpleGUITest.py
+++ PySimpleGUITest.py
@@ -1,8 +1,8 @@
 import PySimpleGUI as sg
 layout = [[sg.Text("What's your name?")],
-          [sg.Input(key="input")],
-          [sg.Text(size=(40,1), key="output")],
-          [sg.Button("Ok"), sg.Button("Quit")]]
+          [sg.Input(key = "input")],
+          [sg.Text(size = (40,1), key = "output")],
+          [sg.Button("Ok"), sg.Button("Exit")]]
 window = sg.Window("Introduction", layout)
 while True:
     event, values = window.read()
