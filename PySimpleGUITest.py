e0c0434f6e9c43364e162c19e4d9e7ddd6635d3f
Oliver King <116508612+oliverk1@users.noreply.github.com>
2022-11-23 10:05:03 +0000

    Add files via upload

---
+++ PySimpleGUITest.py
@@ -0,0 +1,12 @@
+import PySimpleGUI as sg
+layout = [[sg.Text("What's your name?")],
+          [sg.Input(key="input")],
+          [sg.Text(size=(40,1), key="output")],
+          [sg.Button("Ok"), sg.Button("Quit")]]
+window = sg.Window("Introduction", layout)
+while True:
+    event, values = window.read()
+    if event == sg.WINDOW_CLOSED or event == "Quit":
+        break
+    window["output"].update("Hi " + values["input"] + "!")
+window.close()
